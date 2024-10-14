a3cbbaa54364285d32b65f119eb21ef5d7cca237
Bruno <b-d.dupuis@laposte.net>
2024-10-14 22:49:07 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<script setup>
+// Import du tableau contenant la liste des animaux
+import data from './assets/data.json'
+
+// Import du composant
+import Card from './components/Card.vue'
+</script>
+
+<template>
+  <main>
+    <h1>Animals for adoption</h1>
+
+    <div class="container">
+      <!-- Appel du composant 10 fois : une fois par animal -->
+      <Card :animalInfos="data[0]" />
+      <Card :animalInfos="data[1]" />
+      <Card :animalInfos="data[2]" />
+      <Card :animalInfos="data[3]" />
+      <Card :animalInfos="data[4]" />
+      <Card :animalInfos="data[5]" />
+      <Card :animalInfos="data[6]" />
+      <Card :animalInfos="data[7]" />
+      <Card :animalInfos="data[8]" />
+      <Card :animalInfos="data[9]" />
+    </div>
+  </main>
+</template>
+
+<style scoped>
+.container {
+  max-width: 1024px;
+  margin: 0 auto;
+  /* border: 3px solid red; */
+}
+main {
+  padding: 50px;
+  min-height: 100vh;
+  background-color: var(--main-bgc);
+
+  /* border: 2px solid yellow; */
+}
+main > div {
+  display: flex;
+  gap: 15px;
+  flex: 1;
+  /* width: 65vw; */
+  flex-wrap: wrap;
+  /* border: 5px solid orange; */
+}
+main h1 {
+  text-align: center;
+}
+/* ======================== */
+/* ==== MEDIA QUERRIES ==== */
+/* ======================== */
+
+@media (max-width: 660px) {
+  .card {
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,75 @@
+<script setup>
+import Details from '../components/Details.vue'
+
+// Déclaration de la props qui contient les informations d'un animal
+const props = defineProps({
+  animalInfos: Object
+})
+</script>
+
+<template>
+  <div
+    class="card"
+    :class="{
+      opacity: animalInfos.alreadyAdopted === true
+    }"
+  >
+    <div>
+      <Details :animalInfos />
+      <!-- <h2>{{ animalInfos.name }}</h2>
+      <p><span>Espèce</span> : {{ animalInfos.species }}</p>
+      <p><span>Age</span> : {{ animalInfos.age }}</p> -->
+    </div>
+
+    <p class="description">{{ animalInfos.description }}</p>
+  </div>
+</template>
+
+<style scoped>
+.opacity {
+  opacity: 0.3;
+  border: 1px solid red;
+}
+.card {
+  background-color: var(--bgc);
+  padding: 15px;
+  border-radius: 5px;
+  width: calc((100% - 15px) / 2);
+  display: flex;
+  justify-content: center;
+  gap: 10px;
+  box-shadow: 0px 0px 7px var(--shadow-color);
+  /* border: 3px solid blue; */
+}
+
+.card div {
+  width: 150px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+
+  /* border: 3px solid red; */
+}
+.description {
+  flex: 1;
+  /* border: 1px solid green; */
+}
+span {
+  text-decoration: underline;
+  color: gray;
+  /* border: 1px solid purple; */
+}
+.card p {
+  color: var(--text-color);
+}
+/* ======================== */
+/* ==== MEDIA QUERRIES ==== */
+/* ======================== */
+
+@media (max-width: 900px) {
+  .card {
+    width: 100%;
+    flex-wrap: wrap;
+  }
+}
+</style>
